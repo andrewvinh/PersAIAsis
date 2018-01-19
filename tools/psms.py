5bf72c415f6a83b337a8b2e42c9c534f3e279435
Andrew Vinh <andrew_vinh@hotmail.com>
2018-01-18 20:37:07 -0800

    File added

---
+++ tools/psms.py
@@ -0,0 +1,10 @@
+"""
+curl 'https://api.twilio.com/2010-04-01/Accounts/ACcdd91d351f6c22afb8f270c755b0c8e1/Messages.json' -X POST \
+--data-urlencode 'To=+14086938190' \
+--data-urlencode 'From=+14087097097' \
+-u ACcdd91d351f6c22afb8f270c755b0c8e1:938adea889ca04099087eed0c54c0f30
+"""
+
+def textMyself(words):
+    print "Texting the main body!"
+    

af3e8a5767c568bc4b81e4be3dfc83cc8eadb606
Andrew Vinh <andrew_vinh@hotmail.com>
2018-01-23 19:39:59 -0800

    Updated configs

--- tools/psms.py
+++ tools/psms.py
@@ -1,17 +1,17 @@
 """
-curl 'https://api.twilio.com/2010-04-01/Accounts/ACcdd91d351f6c22afb8f270c755b0c8e1/Messages.json' -X POST \
+curl 'https://api.twilio.com/2010-04-01/Accounts/AccountSID/Messages.json' -X POST \
 --data-urlencode 'To=+14086938190' \
 --data-urlencode 'From=+14087097097' \
--u ACcdd91d351f6c22afb8f270c755b0c8e1:AuthToken
+-u AccountSID:AuthToken
 """
 from imports import *
 from twilio.rest import Client
 
 
 # Your Account SID from twilio.com/console
-account_sid = "ACcdd91d351f6c22afb8f270c755b0c8e1"
+account_sid = pdata.getConfig("twilID")
 # Your Auth Token from twilio.com/console
-auth_token  = pdata.getConfig("twilioAuth")
+auth_token  = pdata.getConfig("twilAuth")
 
 self = "+14087097097"
 myself = "+14086938190"
@@ -21,11 +21,12 @@ kris = "+14086213791"
 def textMyself(words):
     print "Texting the main body: ", words
     client = Client(account_sid, auth_token)
+    """
     message = client.messages.create(
     to = myself, 
     from_ = self,
     body = ''.join(str(e+" ") for e in words))
-    print(message.sid)
+    print(message.sid)"""
 
 def addContact(new):
     print "Adding to contacts: ", new
